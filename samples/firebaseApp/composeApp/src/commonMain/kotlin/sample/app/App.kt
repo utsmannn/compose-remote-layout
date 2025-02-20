d204ea97aa3f6d6ed43296bc134aa93a32c1aa86
utsman <utsmannn@gmail.com>
2025-02-20 22:17:31 +0700

    feat: Update GPG key handling in publish workflow and add status bar padding to Firebase sample
    This commit includes two primary changes:
    
    -   **Github Workflow Update:** Modifies the `publish.yaml` workflow file to handle the GPG key. The GPG key is now decoded and saved to the `${{ github.workspace }}` directory, instead of a fixed `~/.gradle/` location, to prevent permission issues.
    -   **Firebase Sample Update:** In `samples/firebaseApp/composeApp/src/commonMain/kotlin/sample/app/App.kt`, adds status bar padding to the `Scaffold` component to prevent content from being obscured by the status bar.

--- samples/firebaseApp/composeApp/src/commonMain/kotlin/sample/app/App.kt
+++ samples/firebaseApp/composeApp/src/commonMain/kotlin/sample/app/App.kt
@@ -1,10 +1,15 @@
 package sample.app
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import com.utsman.composeremote.DynamicLayout
 import com.utsman.composeremote.LayoutParser.parseLayoutJson
 
@@ -54,7 +59,12 @@ val textJsonDefault =
 @Suppress("ktlint:standard:function-naming")
 @Composable
 fun App(textJson: String = textJsonDefault) {
-    Scaffold {
+    Scaffold(
+        modifier = Modifier
+            .padding(
+                WindowInsets.statusBars.asPaddingValues().calculateTopPadding(),
+            ),
+    ) {
         val layoutNode by remember(textJson) { mutableStateOf(parseLayoutJson(textJson)) }
 
         DynamicLayout(layoutNode) { clickId ->
