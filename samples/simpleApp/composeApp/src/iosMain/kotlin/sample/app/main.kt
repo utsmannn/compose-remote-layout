4b1af45f5a6aedd062d8928bb45e0c7c754fa255
utsman <utsmannn@gmail.com>
2025-03-14 01:28:00 +0700

    feat: Implement Grid component and enhance text properties, update custom node registration
    - Introduces the Grid component for creating grid layouts.
    - Enhances text properties with maxLines, minLines, and overflow.
    - Registers custom nodes through a shared utility.
    - Updates the Home constant to utilize the Grid component and new text properties.
    - Removes unused imports in iOS and Android main files.
    - Modifies the json schema to add Grid component, span attribute in layout modifier and text properties.

--- samples/simpleApp/composeApp/src/iosMain/kotlin/sample/app/main.kt
+++ samples/simpleApp/composeApp/src/iosMain/kotlin/sample/app/main.kt
@@ -1,36 +1,18 @@
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import com.utsman.composeremote.CustomNodes
 import platform.UIKit.UIViewController
 import sample.app.App
+import shared.compose.Shared
 
 fun MainViewController(): UIViewController {
-    CustomNodes.register("user") { param ->
-        Column(
-            modifier = param.modifier,
-        ) {
-            Text(
-                text = param.data["name"] ?: "unknown",
-                style = MaterialTheme.typography.body1,
-            )
-            Spacer(Modifier.height(8.dp))
-            Text(
-                text = param.data["phone"] ?: "unknown",
-                style = MaterialTheme.typography.caption,
-            )
-        }
-    }
+    Shared.registerCustomNode()
 
     return ComposeUIViewController {
         MaterialTheme {
